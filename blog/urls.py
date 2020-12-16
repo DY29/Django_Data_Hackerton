a91966a66826ce0f72333d7e9c552870272d0d17
surisuri <kwondy2801@gmail.com>
2020-12-17 01:02:58 +0900

    home edit

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('intro/', views.intro, name='intro'),
     path('emojomo/', views.emojomo, name='emojomo'),
 ]
