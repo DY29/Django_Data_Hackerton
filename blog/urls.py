73939d4767d28dc1483182e4d74fc1895a08eb5e
surisuri <kwondy2801@gmail.com>
2020-12-20 13:40:08 +0900

    map yaksuto switch

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.map, name='map'),
     path('intro/', views.intro, name='intro'),
     path('emojomo/', views.emojomo, name='emojomo'),
     path('team/', views.team, name='team'),
-    path('yaksuto/', views.yaksuto, name='yaksuto'),
+    path('map/', views.map, name='map'),
+    path('', views.home, name='home'),
 ]
 
